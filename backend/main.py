0eb62a0551cdec71b523d8cd679c5ae91036bdca
Gyimah Gideon <gideongyimah19@gmail.com>
2024-07-11 18:22:38 +0000

    great

--- backend/main.py
+++ backend/main.py
@@ -54,7 +54,7 @@ app = FastAPI(
 origins = [
     "https://hackathon-37g0uk0y2-gideon-gyimahs-projects.vercel.app", 
     "https://hackathon-75vru3nwf-gideon-gyimahs-projects.vercel.app",
-    "https://hackathon-97icvvxb7-gideon-gyimahs-projects.vercel.app",
+    "https://hackathon-qu1xdiq64-gideon-gyimahs-projects.vercel.app",
     "https://hackathon-bzxj6yoz3-gideon-gyimahs-projects.vercel.app",
     "http://localhost:3000",
 ]
